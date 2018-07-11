a837407f51ca443c6dbaa479aa653373ab1e3398
Alden Neely <alden.s.neel@gmail.com>
2018-07-11 15:35:27 -0500

    mobile

--- _javascript/main.js
+++ _javascript/main.js
@@ -18,39 +18,50 @@ const renderMember = (json) => `
 `
 
 $(document).ready(() => {
-  $.ajax({
-    url: 'members.yml',
-    dataType: 'text',
-  }).done((yml) => {
-    const data = jsyaml.load(yml);
+  if (window.location.href.indexOf('index.html') > -1 ) {
+    $.ajax({
+      url: 'members.yml',
+      dataType: 'text',
+    }).done((yml) => {
+      const data = jsyaml.load(yml);
 
-    let rowNum = 0 ;
-    let row;
+      let rowNum = 0 ;
+      let row;
 
-    for (let member of data.members.slice(0, 4)) {
-      if (rowNum == 0) {
-        row = document.createElement('div');
-        row.classList.add('columns');
-        $('#members').append(row);
+      for (let member of data.members.slice(0, 4)) {
+        if (rowNum == 0) {
+          row = document.createElement('div');
+          row.classList.add('columns');
+          $('#members').append(row);
+        }
+
+        const html = renderMember(member) 
+        const div = document.createElement('div');
+        div.classList.add('column');
+        div.innerHTML = html;
+        $(row).append(div);
+        rowNum = (rowNum + 1) % 4;
       }
 
-      const html = renderMember(member) 
-      const div = document.createElement('div');
-      div.classList.add('column');
-      div.innerHTML = html;
-      $(row).append(div);
-      rowNum = (rowNum + 1) % 4;
-    }
+      while(rowNum != 0) {
+        const div = document.createElement('div');
+        div.classList.add('column');
+        $(row).append(div);
+        rowNum = (rowNum + 1) % 4;
+      }
 
-    while(rowNum != 0) {
-      const div = document.createElement('div');
-      div.classList.add('column');
-      $(row).append(div);
-      rowNum = (rowNum + 1) % 4;
-    }
+      $('#members').removeClass('is-loading');
+    }).fail((e, status, error) => {
+      console.debug(e + status + error);
+    });
+  }
 
-    $('#members').removeClass('is-loading');
-  }).fail((e, status, error) => {
-    console.debug(e + status + error);
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function() {
+
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
   });
+
 });
